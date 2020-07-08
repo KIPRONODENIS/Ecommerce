e71d30a6fcef8116a40fc5915bcea030d541ae8f
kiprono Denis <kipronokemei98@gmail.com>
2020-07-08 21:18:14 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+    	colors: {
+    		primary:'#5856d6',
+    		secondary:'#ff2d55',
+    		dark:'#aaa'
+    	}
+    },
+  },
+  variants: {},
+  plugins: [],
+}
